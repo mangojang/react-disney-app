58f80b7c58bd876d3c4d1b40f14d73afad4bd6ad
mangojang994 <mangojang994@gmail.com>
2023-05-26 16:21:11 +0900

    Add : Category component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import Banner from '@/components/Banner';
+import Category from '@/components/Category';
 
 export default function Home() {
-	return <Banner />;
+	return (
+		<>
+			<Banner />
+			<Category />
+		</>
+	);
 }

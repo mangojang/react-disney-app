0b61fa8ac14896f65cf887aba193ca6e519db986
mangojang994 <mangojang994@gmail.com>
2023-06-01 14:50:20 +0900

    Update : profile image 적용 로직 변경

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -20,7 +20,9 @@ const Nav = () => {
 
 	const [show, setShow] = useState(false);
 	const [searchVal, setSearchVal] = useState(searchTerm || '');
-	const { isLoggedIn, userInfo } = useAppSelector(state => state.user);
+	const { isLoggedIn } = useAppSelector(state => state.user);
+
+	const [photoUrl, setPhotoUrl] = useState('');
 
 	const handleScroll = useCallback(() => {
 		if (window.scrollY > 50) {
@@ -28,6 +30,11 @@ const Nav = () => {
 		} else {
 			setShow(false);
 		}
+	}, []);
+
+	useEffect(() => {
+		const photourl = localStorage.getItem('photoUrl');
+		photourl && setPhotoUrl(photourl);
 	}, []);
 
 	useEffect(() => {
@@ -47,13 +54,10 @@ const Nav = () => {
 		};
 	}, []);
 
-	const handleChange = useCallback(
-		(e: React.ChangeEvent<HTMLInputElement>) => {
-			setSearchVal(e.target.value);
-			router.replace(`/search?str=${e.target.value}`);
-		},
-		[router]
-	);
+	const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchVal(e.target.value);
+		router.replace(`/search?str=${e.target.value}`);
+	}, []);
 
 	const handleAuth = useCallback((e: React.MouseEvent<HTMLElement>) => {
 		e.preventDefault();
@@ -63,6 +67,10 @@ const Nav = () => {
 				dispatch(setUser(user));
 				dispatch(setLoggedIn(true));
 				setCookie('uid', user.uid, 1);
+				if (user.photoURL) {
+					setPhotoUrl(user.photoURL);
+					localStorage.setItem('photoUrl', user.photoURL);
+				}
 			})
 			.catch(error => {
 				alert(`잠시후 다시 시도해 주세요.\n${error.message}`);
@@ -76,6 +84,8 @@ const Nav = () => {
 				dispatch(setUser({}));
 				dispatch(setLoggedIn(false));
 				delCookie('uid');
+				setPhotoUrl('');
+				localStorage.removeItem('photoUrl');
 			})
 			.catch(error => {
 				console.log(error);
@@ -97,7 +107,7 @@ const Nav = () => {
 						placeholder="검색어를 입력 해주세요."
 					/>
 					<div className="profile">
-						<Image alt="profile image" className="userimg" src={userInfo.photoURL} width={48} height={48} />
+						<Image alt="profile image" className="userimg" src={photoUrl} width={48} height={48} />
 						<div className="dropdown">
 							<span className="btn--logout" onClick={handleLogout}>
 								Logout

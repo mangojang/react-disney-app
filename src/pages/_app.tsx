82c6fa0a2a75269578beb82dfdf0fd39a1e9abe3
mangojang994 <mangojang994@gmail.com>
2023-06-01 14:52:15 +0900

    Fix : charset 오타

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ const MyApp: FC<AppProps> = function MyApp({ Component, pageProps }) {
 		<>
 			<Head>
 				<title>react-disney-app</title>
-				<meta charset="UTF-8" />
+				<meta charSet="UTF-8" />
 				<meta
 					name="viewport"
 					content="width=device-width,initial-scale=1.0,minimum-scale=0,maximum-scale=10,user-scalable=yes"
